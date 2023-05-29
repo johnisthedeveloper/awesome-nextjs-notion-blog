af120202dc67c8bf43c20358afc1717d458553b3
JohnDeveloper <johnisthedeveloper@gmail.com>
2023-05-29 01:37:01 -0400

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,27 +1,27 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '6a76fdd9ec6d4ae2855aa790d620eec4',
+  rootNotionPageId: 'da928effd5a645b3a1ba00273eeeb5af',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: '菜饼的技术博客',
+  name: 'John的独立博客',
   domain: 'szufrank.top',
-  author: '菜饼同学',
+  author: '老John在北美',
 
   // open graph metadata (optional)
-  description: '菜饼同学: 专注硬核技术，校招打法，个人认知体系成长',
-  socialImageTitle: '菜饼同学',
+  description: '老John在北美: 专注硬核技术，校招打法，个人认知体系成长',
+  socialImageTitle: '老John在北美',
   socialImageSubtitle: 'Hello World! 👋',
 
   // social usernames (optional)
   twitter: null,
   github: 'frankcbliu',
-  wechatPublicName: '菜饼不菜',
+  wechatPublicName: '老John在北美',
   wechatPublicURL: 'https://mp.weixin.qq.com/s?__biz=MzkxMzIyOTI1Mg==&mid=2247486027&idx=1&sn=2840f0b8cfdc982b9095dd18ec9892e5',
-  notionPublic: 'https://frankcbliu.notion.site/6a76fdd9ec6d4ae2855aa790d620eec4',
+  notionPublic: 'https://johndeveloper.notion.site/da928effd5a645b3a1ba00273eeeb5af',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
